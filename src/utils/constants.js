23e2b3a8bb561c7fe96e4823427d6dd032f53681
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-14 17:10:30 +0530

    FEAT: add constants
    - some typos could be made during coding
    - and to get rid of those issue , we can make constant and use them

---
+++ src/utils/constants.js
@@ -0,0 +1,16 @@
+export const UserRolesEnum = {
+  ADMIN: "admin",
+  PROJECT_ADMIN: "project_admin",
+  MEMBER: "member",
+};
+
+export const AvailableUserRole = Object.values(UserRolesEnum);
+// ["admin","project_admin","member"]
+
+export const TaskStatusEnum = {
+  TODO: "todo",
+  IN_PROGRESS: "in_progress",
+  DONE: "done",
+};
+
+export const AvailableTaskStatuses = Object.values(TaskStatusEnum);
