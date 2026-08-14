f50ee75e81c59131e90df0825f63a5902ae809ef
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-14 20:17:49 +0530

    chore: import connectDB function to connect to the mongodb

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import { connectDB } from "./db/index.js";
 
-dotenv.config({
-  path: "./.env",
-});
+dotenv.config({ path: "./.env" });
 
 const PORT = process.env.PORT || 8000;
 
