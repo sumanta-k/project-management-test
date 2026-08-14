a94a3fa22dfa7bd076ed2e6ffde93ce6985491a7
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-14 11:38:18 +0530

    feat: config cors
    - what origin to give access
    - accept cookies/auth via credentials
    - methods (get,post...)
    - headers-allowed

--- src/app.js
+++ src/app.js
@@ -5,6 +5,14 @@ const app = express();
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+app.use(
+  cors({
+    origin: process.env.ORIGIN?.split(",") || "http://127.0.0.1:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
