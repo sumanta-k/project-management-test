31b24e532da6935352d44ec2c4cd03004981351c
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-14 11:16:50 +0530

    chore: add limit of 16kb of json data

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "16kb" }));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
