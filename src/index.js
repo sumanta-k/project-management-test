8086d5ac83b73c208611f76d5071b28f0949dd8b
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-14 10:22:14 +0530

    import app from app.js and configure it to run our server.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import dotenv from "dotenv";
-import express from "express";
+import app from "./app.js";
+
 dotenv.config({
   path: "./.env",
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log("App is running on port " + PORT);
 });
