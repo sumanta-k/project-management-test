39fa40a3440b9aa7e5031888c64aa7c4315bb309
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-14 11:20:39 +0530

    feat: add public folder to serve publically viewable content to serve.

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
