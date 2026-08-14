6cc907e48e8a0cb7e394108dcb0b1df39d9e8382
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-14 11:19:35 +0530

    chore: add limit of 16kb

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 const app = express();
 
 app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
