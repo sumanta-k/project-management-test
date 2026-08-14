fe1fd21cf59dfc2a14a102522a95882f9c5a5538
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-14 11:17:40 +0530

    chore: add url-encoded type
    - %20 for spacing
    - %2 for "/"
    and so on. these gonna be acceptable via this extended true option of
    urlencoded.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 const app = express();
 
 app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
