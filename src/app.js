2073039b75a5722ecdfe5983779f111aea7f886f
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-15 09:39:59 +0530

    add middleware which will tell to go to/api/v1/healthcheck

--- src/app.js
+++ src/app.js
@@ -14,6 +14,10 @@ app.use(
   })
 );
 
+// import routes
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+app.use("/api/v1/healthcheck", healthCheckRouter);
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
