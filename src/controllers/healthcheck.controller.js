3ad5f8a74f77ef6624bfcb72cf23a4088eb2d857
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-15 10:46:38 +0530

    REFACTOR: update the health check controller

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -14,4 +14,10 @@ const healthCheck = async (req, res, next) => {
 
 */
 
+const healthCheck = asyncHandler(async (req, res) => {
+  res
+    .status(200)
+    .json(new ApiResponse(200, { data: "health check ok" }, "bingo"));
+});
+
 export { healthCheck };
