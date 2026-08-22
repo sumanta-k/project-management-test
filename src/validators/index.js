719093e1299eae9fb98338d6260167ad5e1fb2c7
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-22 10:24:34 +0530

    feat: validate username, email, password

---
+++ src/validators/index.js
@@ -0,0 +1,27 @@
+import { body } from "express-validator";
+
+const userRegisterValidator = () => {
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("email is required")
+      .isEmail()
+      .withMessage("email is invalid"),
+    body("username")
+      .trim()
+      .isEmpty()
+      .withMessage("user name is required")
+      .isLowercase()
+      .withMessage("user name must be in lower case")
+      .isLength({ min: 3 })
+      .withMessage("user name must be at least 3 characters long"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("password should not be empty"),
+    body("fullName").optional().trim(),
+  ];
+};
+
+export { userRegisterValidator };
