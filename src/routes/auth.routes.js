c14a8729df6d4cf63951142e0e6ed89ad7a77f7d
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-22 11:22:38 +0530

    set login route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,4 +1,4 @@
-import { registerUser } from "../controllers/auth.controllers.js";
+import { registerUser, login } from "../controllers/auth.controllers.js";
 import { validate } from "../middlewares/validator.middleware.js";
 import { userRegisterValidator } from "../validators/index.js";
 import { Router } from "express";
@@ -6,5 +6,6 @@ import { Router } from "express";
 const router = Router();
 
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
+router.route("/login").post(login);
 
 export default router;
