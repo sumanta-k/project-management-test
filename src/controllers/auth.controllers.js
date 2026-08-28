97aeb88c08717630e99a6e22ab7a7a7865a6f344
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-28 07:56:46 +0530

    feat(auth): add getCurrentUser endpoint to retrieve current user details
    - Added new getCurrentUser controller function that fetches the current
      aauthenticated user
    - The function returns the user data via ApiResponse

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -130,4 +130,11 @@ const logoutUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "User logged out"));
 });
 
+const getCurrentUser = asyncHandler(async (req, res) => {
+  return;
+  res
+    .status(200)
+    .json(new ApiResponse(200, req.user, "current user fetched successfully"));
+});
+
 export { registerUser, login, logoutUser };
