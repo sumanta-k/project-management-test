6b160395b1438835d137f6b30812b89a35a022f1
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-08-28 08:35:28 +0530

    fix(auth): remove premature return statement in getCurrentUser endpoint

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -131,8 +131,7 @@ const logoutUser = asyncHandler(async (req, res) => {
 });
 
 const getCurrentUser = asyncHandler(async (req, res) => {
-  return;
-  res
+  return res
     .status(200)
     .json(new ApiResponse(200, req.user, "current user fetched successfully"));
 });
