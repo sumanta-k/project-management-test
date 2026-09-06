eb2d14e619b7d449afa8900fac6a1dd8e7197a01
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-09-06 07:53:46 +0530

    feat(routes): add refresh token route
    It refreshes the access-token.

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -24,6 +24,7 @@ const router = Router();
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
 router.route("/login").post(userLoginValidator(), validate, login);
 router.route("/verify-email/:verificationToken").get(verifyEmail);
+router.route("refresh-token").post(refreshAccessToken);
 
 // NOTE: secure routes
 router.route("/logout").post(verifyJWT, logoutUser);
