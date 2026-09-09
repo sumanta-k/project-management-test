6031899631a2eca5b185fba320914c483bbd33b2
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-09-09 16:01:09 +0530

    feat(controller): add project controller

---
+++ src/controllers/project.controllers.js
@@ -0,0 +1,54 @@
+import { User } from "../models/user.models.js";
+import { Project } from "../models/project.models.js";
+import { ProjectMember } from "../models/projectmember.models.js";
+import { ApiResponse } from "../utils/api-response.js";
+import { ApiError } from "../utils/api-error.js";
+import { asyncHandler } from "../utils/async-handler.js";
+
+const getProjects = asyncHandler(async (req, res) => {
+  // TEST: get all existing projects
+});
+
+const getProjectById = asyncHandler(async (req, res) => {
+  // TEST: get project by id
+});
+
+const createProject = asyncHandler(async (req, res) => {
+  // TEST: create project
+});
+
+const updateProject = asyncHandler(async (req, res) => {
+  // TEST: update project by id
+});
+
+const deleteProject = asyncHandler(async (req, res) => {
+  // TEST: delete project by id
+});
+
+const addMembersToProject = asyncHandler(async (req, res) => {
+  // TEST: add members to project
+});
+
+const getProjectMembers = asyncHandler(async (req, res) => {
+  // TEST: get project members
+});
+
+const updateMemberRole = asyncHandler(async (req, res) => {
+  // TEST: update member role
+});
+
+const deleteMember = asyncHandler(async (req, res) => {
+  // TEST: delete member from the project
+});
+
+export {
+  getProjects,
+  getProjectById,
+  createProject,
+  deleteProject,
+  updateProject,
+  addMembersToProject,
+  getProjectMembers,
+  updateMemberRole,
+  deleteMember,
+};
