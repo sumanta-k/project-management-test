6cb40dd17fb622fa0daf1fb0040dd31054e1ee15
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-09-10 07:28:41 +0530

    feat(controller): add create project controller

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -14,7 +14,23 @@ const getProjectById = asyncHandler(async (req, res) => {
 });
 
 const createProject = asyncHandler(async (req, res) => {
-  // TEST: create project
+  const { name, description } = req.body;
+
+  const project = await Project.create({
+    name,
+    description,
+    createdBy: new mongoose.Types.ObjectId(req.user._id),
+  });
+
+  await ProjectMember.create({
+    user: new mongoose.Types.ObjectId(req.user._id),
+    project: new mongoose.Types.ObjectId(project._id),
+    role: UserRolesEnum.ADMIN,
+  });
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, project, "project created successfully"));
 });
 
 const updateProject = asyncHandler(async (req, res) => {
